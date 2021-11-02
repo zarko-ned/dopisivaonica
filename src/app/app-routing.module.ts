afd73f670f3f9eba4acbf4fb64329c900567c2ee
unknown <zarkoned@outlook.com>
2021-11-02 10:25:03 +0100

    redeploy on new account

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { ChatComponent } from './pages/chat/chat.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  {
+    path: 'chat', canActivate: [AuthGuard],
+    children: [
+      { path: '', component: ChatComponent },
+      { path: ':chatroomId', component: ChatComponent }
+    ]
+  },
+  { path: '**', redirectTo: '/login' }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
